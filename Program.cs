ac0be41f5203937998af65df532121980d872ff7
JAD MASHTOUB <103596586@student.swin.edu.au>
2021-04-27 13:07:01 +1000

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace practice_assessment
+{
+    class Program
+    {
+        
+            static void Main(string[] args) {
+                int number; 
+                number = int.Parse(Console.ReadLine());
+                    if (number == 10)
+                    Console.WriteLine("Damn. You win! the number was indeed 10");
+                Console.WriteLine("hey there! Lets play a little guessint game. Guess the number between 0 and 25");
+        }
+    }
+}
